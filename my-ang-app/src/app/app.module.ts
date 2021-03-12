be0e66be01c685fd684e5cedd2ee1a496014ff31
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-03-12 18:56:21 +0400

    feat: add components

--- my-ang-app/src/app/app.module.ts
+++ my-ang-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { CoursesPageComponent } from './components/main/courses-page/courses-page.component';
+import { CourseComponent } from './components/main/courses-page/course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    CoursesPageComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
