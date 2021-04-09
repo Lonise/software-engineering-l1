89d6df06495b8efd97c32b8796a1fdbadd4654a4
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-04-09 08:38:27 +0400

    feat: implement tests for order-by-creation-date pipe

--- my-ang-app/src/app/components/main/courses-page/course/duration-course.pipe.spec.ts
+++ my-ang-app/src/app/components/main/courses-page/course/duration-course.pipe.spec.ts
@@ -1,18 +1,18 @@
 import { DurationCoursePipe } from './duration-course.pipe';
 
 describe('DurationCoursePipe', () => {
-	const pipe = new DurationCoursePipe();
+	const durationCoursePipe = new DurationCoursePipe();
 	let durationExample: number;
 
 	it('create an instance', () => {
-		expect(pipe).toBeTruthy();
+		expect(durationCoursePipe).toBeTruthy();
 	});
 	it('should return duration with "min" if duration less than 1h', () => {
 		durationExample = 50;
-		expect(pipe.transform(durationExample)).toEqual('50min');
+		expect(durationCoursePipe.transform(durationExample)).toEqual('50min');
 	});
 	it('Should return duration in minutes and hours if duration more than 59', () => {
 		durationExample = 70;
-		expect(pipe.transform(durationExample)).toEqual('1h 10min');
+		expect(durationCoursePipe.transform(durationExample)).toEqual('1h 10min');
 	});
 });
