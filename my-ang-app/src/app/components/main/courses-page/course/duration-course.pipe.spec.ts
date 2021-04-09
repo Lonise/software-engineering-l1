8522cec0da1fc817464f543435b44cc62ba5f731
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-04-09 07:55:42 +0400

    feat: implement tests for duration pipe

--- my-ang-app/src/app/components/main/courses-page/course/duration-course.pipe.spec.ts
+++ my-ang-app/src/app/components/main/courses-page/course/duration-course.pipe.spec.ts
@@ -1,8 +1,18 @@
 import { DurationCoursePipe } from './duration-course.pipe';
 
 describe('DurationCoursePipe', () => {
+	const pipe = new DurationCoursePipe();
+	let durationExample: number;
+
 	it('create an instance', () => {
-		const pipe = new DurationCoursePipe();
 		expect(pipe).toBeTruthy();
+	});
+	it('should return duration with "min" if duration less than 1h', () => {
+		durationExample = 50;
+		expect(pipe.transform(durationExample)).toEqual('50min');
+	});
+	it('Should return duration in minutes and hours if duration more than 59', () => {
+		durationExample = 70;
+		expect(pipe.transform(durationExample)).toEqual('1h 10min');
 	});
 });
