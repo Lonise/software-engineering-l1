5680c9cd86bd1290ec0450ce4a19b72ca31b2b57
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-04-11 13:33:06 +0400

    refactor: removed tests that check creation of the pipes

--- my-ang-app/src/app/components/main/courses-page/course/duration-course.pipe.spec.ts
+++ my-ang-app/src/app/components/main/courses-page/course/duration-course.pipe.spec.ts
@@ -1,16 +1,14 @@
 import { DurationCoursePipe } from './duration-course.pipe';
 
-describe('DurationCoursePipe', () => {
+describe('DurationCoursePipe transform()', () => {
 	const durationCoursePipe = new DurationCoursePipe();
 	let durationExample: number;
 
-	it('create an instance', () => {
-		expect(durationCoursePipe).toBeTruthy();
-	});
 	it('should return duration with "min" if duration less than 1h', () => {
 		durationExample = 50;
 		expect(durationCoursePipe.transform(durationExample)).toEqual('50min');
 	});
+
 	it('Should return duration in minutes and hours if duration more than 59', () => {
 		durationExample = 70;
 		expect(durationCoursePipe.transform(durationExample)).toEqual('1h 10min');
--- my-ang-app/src/app/components/main/courses-page/search-add/filter-courses-by-input.pipe.spec.ts
+++ my-ang-app/src/app/components/main/courses-page/search-add/filter-courses-by-input.pipe.spec.ts
@@ -1,7 +1,7 @@
 import { FilterCoursesByInputPipe } from './filter-courses-by-input.pipe';
 import { Course } from '../course';
 
-describe('FilterCoursesByInputPipe', () => {
+describe('FilterCoursesByInputPipe transform()', () => {
 	let filterCoursesByInputPip: FilterCoursesByInputPipe;
 	let inputExample: string;
 	let courseListExample: Course[] = [
@@ -35,17 +35,13 @@ describe('FilterCoursesByInputPipe', () => {
 		filterCoursesByInputPip = new FilterCoursesByInputPipe()
 	});
 
-	it('create an instance', () => {
-		expect(filterCoursesByInputPip).toBeTruthy();
-	});
-
 	it('should return courses with current input in the title', () => {
 		inputExample = 's';
 		expect(filterCoursesByInputPip.transform(courseListExample, inputExample).length).toEqual(2);
 	});
 
 	it('should return an empty array if the course list does not contain a course with a title for the current input', () => {
-		inputExample = 'nonexistent course';
+		inputExample = 'nonexistent course title';
 		expect(filterCoursesByInputPip.transform(courseListExample, inputExample)).toEqual([]);
 	});
 
