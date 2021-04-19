354d59abcc88aa820afc1fcf11d943aa5d72418c
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-04-19 17:50:10 +0400

    feat: change changeDetection to onPush

--- my-ang-app/src/app/components/main/courses-page/course/course.component.ts
+++ my-ang-app/src/app/components/main/courses-page/course/course.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 
 import { Course } from '../course';
 
 @Component({
 	selector: 'app-course',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	templateUrl: './course.component.html',
-	styleUrls: ['./course.component.scss']
+	styleUrls: ['./course.component.scss'],
 })
+
 export class CourseComponent implements OnDestroy{
 
 	@Input() course!: Course;
