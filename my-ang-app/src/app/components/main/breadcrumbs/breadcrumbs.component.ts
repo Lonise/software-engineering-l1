659c8eac07d675e62b382de6a285a44574841998
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-04-27 02:13:14 +0400

    feat: implement breadcrumbs

--- my-ang-app/src/app/components/main/breadcrumbs/breadcrumbs.component.ts
+++ my-ang-app/src/app/components/main/breadcrumbs/breadcrumbs.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
 
 @Component({
 	selector: 'app-breadcrumbs',
 	templateUrl: './breadcrumbs.component.html',
 	styleUrls: ['./breadcrumbs.component.scss']
 })
-export class BreadcrumbsComponent { }
+export class BreadcrumbsComponent implements OnInit {
+
+	constructor ( private router: Router ) {}
+	public breadcrumbsPass!: string;
+
+	public updateBreadcrumbsPath(event: Event) {
+		if (event instanceof NavigationEnd) {
+			this.breadcrumbsPass = event.urlAfterRedirects.slice(1).split('/').join(' > ').toUpperCase();
+		}
+	}
+
+	ngOnInit(): void {
+		this.router.events.subscribe(event => this.updateBreadcrumbsPath(event));
+	}
+
+}
