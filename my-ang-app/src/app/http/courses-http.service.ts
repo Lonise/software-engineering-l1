191d70a48dd260b6d23c0ab38d9f0ccfb2458823
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-05-20 15:08:26 +0400

    feat: implement courses http service

---
+++ my-ang-app/src/app/http/courses-http.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Course } from '../components/Interfaces-and-classes/course/course';
+
+@Injectable()
+export class CoursesHttpService {
+
+  constructor(private httpClient: HttpClient) { }
+	private url = 'https://super-courses.herokuapp.com/courses/';
+	public isLoading = false;
+
+	public getCourses( url: string = this.url ): Observable<Course[]>{
+    return this.httpClient.get<Course[]>( url );
+	}
+
+	public getPathOfCourses( start: number, count: number ): Observable<Course[]>{
+    return this.httpClient.get<Course[]>( `${this.url}?start=${start}&count=${count}` );
+	}
+
+	public postCourse( course : Course ): Observable<Course[]>{
+    return this.httpClient.post<Course[]>( this.url, course );
+	}
+
+	public putCourse( courseId: string, course: Course ): Observable<Course[]>{
+		course._id = undefined
+    return this.httpClient.put<Course[]>( this.url + courseId, course );
+	}
+
+	public deleteCourse( courseId: string ): Observable<Course[]>{
+    return this.httpClient.delete<Course[]>( this.url + courseId );
+	}
+
+	public show() {
+		this.isLoading = true
+	}
+	public hide() {
+		this.isLoading = false
+	}
+}
