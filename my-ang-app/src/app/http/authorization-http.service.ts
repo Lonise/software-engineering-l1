74a50f98a1d59a6479b7fa6476d72f0e21f4214a
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-05-20 15:13:25 +0400

    feat: implement auth http service

--- my-ang-app/src/app/http/authorization-http.service.ts
+++ my-ang-app/src/app/http/authorization-http.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IUserProperties } from '../components/Interfaces-and-classes/user/user';
@@ -10,26 +10,32 @@ export class AuthorizationHttpService {
 
 	private url = 'https://super-courses.herokuapp.com/authorization/';
 	private loginPath ='login';
-	private signUpPath ='signip';
+	private signUpPath ='signup';
 	private logoutUpPath ='logout';
+	private options = {
+		headers: new HttpHeaders({
+			'Access-Control-Allow-Credentials': 'true',
+			'withCredentials': 'true'
+		})
+	}
 
-	public getUserByActiveSessionToken(token: string): Observable<any>{
-    return this.httpClient.get(this.url + token);
+	public getUserByActiveSessionToken( token: string ): Observable<any>{
+    return this.httpClient.get( this.url + token );
 	}
 
 	public postAuthorization( userData: IUserProperties ): Observable<any>{
-    return this.httpClient.post(this.url + this.loginPath, userData);
+    return this.httpClient.post( this.url + this.loginPath, userData, this.options);
 	}
 
 	public postAuthentication( userData: IUserProperties ): Observable<any>{
-    return this.httpClient.post(this.url + this.signUpPath, userData);
+    return this.httpClient.post(this.url + this.signUpPath, userData, this.options );
 	}
 
 	public putUpdateUser( token: string, userData: IUserProperties ): Observable<any>{
-    return this.httpClient.put(this.url + token, userData);
+    return this.httpClient.put( this.url + token, userData );
 	}
 
 	public deleteActiveSession(): Observable<any>{
-    return this.httpClient.delete(this.url + this.logoutUpPath);
+    return this.httpClient.delete( this.url + this.logoutUpPath );
 	}
 }
