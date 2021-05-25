8d9813fcf5441c76f85c2b88b4a04f6898a204fb
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-05-25 09:42:16 +0400

    feat: add reactive forms for auth block

--- my-ang-app/src/app/components/authorization/authorization.component.ts
+++ my-ang-app/src/app/components/authorization/authorization.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthorizationService } from './authorization.service';
 
 @Component({
@@ -7,15 +8,62 @@ import { AuthorizationService } from './authorization.service';
 	styleUrls: ['./authorization.component.scss', './login-signup-toggle.scss']
 })
 export class AuthorizationComponent {
+	public authControl!: FormGroup;
 
-	constructor( public authorizationService: AuthorizationService ) { }
+	constructor(
+		public authorizationService: AuthorizationService,
+		private formBuilder: FormBuilder ) {
+
+		this.authControl = this.formBuilder.group({
+		 userName: ['',[
+			 Validators.required,
+			 Validators.minLength(5)
+		 ]],
+		 userEmail: ['',[
+			 Validators.required,
+			 Validators.email,
+			 Validators.pattern(this.authorizationService.validationUserEmail)
+		 ]],
+		 userPassword: ['',[
+			 Validators.required,
+			 Validators.minLength(5)
+		 ]],
+		})
+	 }
+
+	get _name() {
+		return this.authControl.get('userName')
+	}
+
+	get _email() {
+		return this.authControl.get('userEmail')
+	}
+
+	get _password() {
+		return this.authControl.get('userPassword')
+	}
 
 	public switchCheckbox(): void {
 		this.authorizationService.isLogIn = !this.authorizationService.isLogIn;
+		this.checkUserInput()
+	}
+
+	public isButtonDisabled = true;
+
+	public checkUserInput(): void {
+		this.isButtonDisabled = !!this._password?.invalid;
+		this.isButtonDisabled = this.isButtonDisabled || !!this._email?.invalid
+		if (! this.authorizationService.isLogIn ) {
+			this.isButtonDisabled = this.isButtonDisabled || !!this._name?.invalid
+		}
+	}
+
+	public submitAuth(): void {
+			this.authorizationService.submitAuth( this.authControl );
 	}
 
 	public closeLogIn(): void {
-		if ( this.authorizationService.isAuthorized) {
+		if ( this.authorizationService.isAuthorized ) {
 			this.authorizationService.closeLogIn();
 		}
 	}
