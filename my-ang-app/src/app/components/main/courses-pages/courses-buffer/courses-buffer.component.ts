60913438400947cacf8e4ddd79ae0abf964b5833
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-06-07 10:42:41 +0400

    feat: implement searching courses ngrx

--- my-ang-app/src/app/components/main/courses-pages/courses-buffer/courses-buffer.component.ts
+++ my-ang-app/src/app/components/main/courses-pages/courses-buffer/courses-buffer.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Course } from 'src/app/components/Interfaces-and-classes/course/course';
-import { CoursesStreamService } from 'src/app/http/courses-stream.service';
 import { CoursesListService } from '../courses-list.service';
 
 @Component({
@@ -10,13 +9,8 @@ import { CoursesListService } from '../courses-list.service';
 })
 export class CoursesBufferComponent {
 
-	constructor(
-		public coursesStreamService: CoursesStreamService,
-		public coursesList: CoursesListService ) { }
+	constructor( public coursesList: CoursesListService ) { }
 
-	public searchCourses(currentInput: string): void {
-		this.coursesStreamService.Courses$.next(currentInput);
-	}
 	public openEditToCourse( course: Course ): void {
 		this.coursesList.openEditCourse(course);
 	}
