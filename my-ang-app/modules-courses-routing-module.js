31e47970871090d3f870f6323f470e7af71fbd8c
Loshkarev Nikita <loshkarev.n@inbox.ru>
2021-06-10 12:04:11 +0400

    fix: fix build errors

--- my-ang-app/modules-courses-routing-module.js
+++ my-ang-app/modules-courses-routing-module.js
@@ -11,13 +11,8 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CoursesRoutingModule", function() { return CoursesRoutingModule; });
 /* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/router */ "tyNb");
-<<<<<<< HEAD
 /* harmony import */ var _components_main_courses_pages_add_course_page_add_course_page_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/main/courses-pages/add-course-page/add-course-page.component */ "/RYb");
 /* harmony import */ var _components_main_courses_pages_courses_buffer_courses_buffer_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/main/courses-pages/courses-buffer/courses-buffer.component */ "9rZT");
-=======
-/* harmony import */ var _components_main_courses_pages_course_list_page_course_list_page_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/main/courses-pages/course-list-page/course-list-page.component */ "ODOa");
-/* harmony import */ var _components_main_courses_pages_add_course_page_add_course_page_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/main/courses-pages/add-course-page/add-course-page.component */ "/RYb");
->>>>>>> a21cc9de6cc2d0377528ad2b6e8647667b11a7d8
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ "fXoL");
 
 
@@ -25,15 +20,9 @@ __webpack_require__.r(__webpack_exports__);
 
 
 const coursesRoutes = [
-<<<<<<< HEAD
     { path: '', component: _components_main_courses_pages_courses_buffer_courses_buffer_component__WEBPACK_IMPORTED_MODULE_2__["CoursesBufferComponent"] },
     { path: 'new', component: _components_main_courses_pages_add_course_page_add_course_page_component__WEBPACK_IMPORTED_MODULE_1__["AddCoursePageComponent"] },
     { path: ':id', component: _components_main_courses_pages_add_course_page_add_course_page_component__WEBPACK_IMPORTED_MODULE_1__["AddCoursePageComponent"] },
-=======
-    { path: '', component: _components_main_courses_pages_course_list_page_course_list_page_component__WEBPACK_IMPORTED_MODULE_1__["CoursesListPageComponent"] },
-    { path: 'new', component: _components_main_courses_pages_add_course_page_add_course_page_component__WEBPACK_IMPORTED_MODULE_2__["AddCoursePageComponent"] },
-    { path: ':id', component: _components_main_courses_pages_add_course_page_add_course_page_component__WEBPACK_IMPORTED_MODULE_2__["AddCoursePageComponent"] },
->>>>>>> a21cc9de6cc2d0377528ad2b6e8647667b11a7d8
 ];
 class CoursesRoutingModule {
 }
